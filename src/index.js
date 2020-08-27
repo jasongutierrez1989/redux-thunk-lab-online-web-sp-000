275ae9f130cefd051e0afb62b7cd6d6eae5cff8c
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-08-27 21:06:48 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
- 
+
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import catsReducer from './reducers/catsReducer.js';
- 
+
 const store = createStore(catsReducer, applyMiddleware(thunk))
- 
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
